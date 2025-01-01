c5f17a2d802a3e7712c82ea252af718a9fb30547
Vishaal J <vishaal@thereciprocalsolutions.com>
2025-01-01 07:01:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app")
-const PORT = process.env.PORT || 6401
+const PORT = process.env.PORT ?? 6401
 
 app.listen(PORT,()=>{
     console.log(`Server running at http://localhost:${PORT}`);
